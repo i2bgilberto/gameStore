d225e726ac09414cb366f6d39da65a05d217d0c4
Ramon Alcala <i2bgilberto@gmail.com>
2024-06-26 17:38:40 -0500

    Adding emoji and changin shipping with static data!

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,8 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
+
 interface Props {
   game: Game;
 }
@@ -27,7 +29,9 @@ const GameCard = ({ game }: Props) => {
             />
             <CriticScore score={game.metacritic} />
           </HStack>
-          <Heading fontSize="1xl">{game.name}</Heading>
+          <Heading fontSize="1xl">
+            {game.name} <Emoji rating={game.rating_top} />
+          </Heading>
         </CardBody>
       </Card>
     </>
